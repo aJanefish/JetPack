8ca9872cb31e87687ae521b2556208b5dec7aedd
YuZhang <641519166@qq.com>
2020-12-09 08:37:10 +0800

    add 20201209 01

--- databindingdemo/src/main/java/com/example/databindingdemo/MainActivity.java
+++ databindingdemo/src/main/java/com/example/databindingdemo/MainActivity.java
@@ -13,6 +13,8 @@ import com.example.databindingdemo.activity.OtherActivity;
 import com.example.databindingdemo.activity.RecyclerViewActivity;
 import com.example.databindingdemo.activity.TwoActivity;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -44,6 +46,175 @@ public class MainActivity extends AppCompatActivity {
 
     //RecyclerView的绑定机制
     public void test6(View view) {
-        startActivity(new Intent(this, RecyclerViewActivity.class));
+        Intent intent = new Intent(this, RecyclerViewActivity.class);
+        //     Caused by: android.os.TransactionTooLargeException: data parcel size 1049036 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IActivityManager$Stub$Proxy.startActivity(IActivityManager.java:3754)
+        //byte[] bytes = new byte[1024 * 1024];
+
+        //Caused by: android.os.TransactionTooLargeException: data parcel size 1040844 bytes
+        //byte[] bytes = new byte[1024 * 1024 - 2 * 4096];
+
+        //    android.os.TransactionTooLargeException: data parcel size 1011236 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+        //byte[] bytes = new byte[1024 * 1024 - 10 * 4096];
+
+
+        //byte[] bytes = new byte[1024 * 1024 - 20 * 4096];
+        //    android.os.TransactionTooLargeException: data parcel size 970276 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+
+
+        //byte[] bytes = new byte[1024 * 1024 - 100 * 1024];
+        //    android.os.TransactionTooLargeException: data parcel size 949796 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+
+
+        //byte[] bytes = new byte[1024 * 1024 - 200 * 1024];
+        //    android.os.TransactionTooLargeException: data parcel size 847396 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+
+
+        //byte[] bytes = new byte[1024 * 1024 - 400 * 1024];
+        //    android.os.TransactionTooLargeException: data parcel size 642596 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+
+
+        //OK  byte[] bytes = new byte[1024 * 1024 - 800 * 1024];
+
+        //OK byte[] bytes = new byte[1024 * 1024 - 600 * 1024];
+
+        //OK byte[] bytes = new byte[1024 * 1024 - 500 * 1024];
+
+        //byte[] bytes = new byte[1024 * 1024 - 450 * 1024];
+        //    android.os.TransactionTooLargeException: data parcel size 540196 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+
+
+        //byte[] bytes = new byte[550 * 1024];
+        //    android.os.TransactionTooLargeException: data parcel size 566820 bytes
+        //        at android.os.BinderProxy.transactNative(Native Method)
+        //        at android.os.BinderProxy.transact(Binder.java:1127)
+        //        at android.app.IApplicationThread$Stub$Proxy.scheduleTransaction(IApplicationThread.java:1777)
+        //        at android.app.servertransaction.ClientTransaction.schedule(ClientTransaction.java:129)
+        //        at com.android.server.am.ClientLifecycleManager.scheduleTransaction(ClientLifecycleManager.java:47)
+        //        at com.android.server.am.ActivityStackSupervisor.realStartActivityLocked(ActivityStackSupervisor.java:1545)
+        //        at com.android.server.am.ActivityStackSupervisor.attachApplicationLocked(ActivityStackSupervisor.java:989)
+        //        at com.android.server.am.ActivityManagerService.attachApplicationLocked(ActivityManagerService.java:7872)
+        //        at com.android.server.am.ActivityManagerService.attachApplication(ActivityManagerService.java:7940)
+        //        at android.app.IActivityManager$Stub.onTransact(IActivityManager.java:198)
+        //        at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:3291)
+        //        at android.os.Binder.execTransact(Binder.java:731)
+
+
+        //OK byte[] bytes = new byte[450 * 1024];
+
+        //OK byte[] bytes = new byte[500 * 1024];
+
+        //OK byte[] bytes = new byte[525 * 1024];
+
+        //byte[] bytes = new byte[540 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 541220 bytes
+
+        //byte[] bytes = new byte[533 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 549412 bytes
+
+
+        //byte[] bytes = new byte[530 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 546340 bytes
+
+        //byte[] bytes = new byte[528 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 544292 bytes
+
+        //byte[] bytes = new byte[526 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 542244 bytes
+
+        //byte[] bytes = new byte[525 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 541220 bytes
+
+        //byte[] bytes = new byte[510 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 525860 bytes
+
+        //byte[] bytes = new byte[505 * 1024];
+        //android.os.TransactionTooLargeException: data parcel size 520740 bytes
+
+        byte[] bytes = new byte[505 * 1024];
+        //OK byte[] bytes = new byte[504 * 1024];
+        //OK byte[] bytes = new byte[503 * 1024];
+
+        //OK byte[] bytes = new byte[502 * 1024];
+
+        // OK byte[] bytes = new byte[500 * 1024];
+
+        Arrays.fill(bytes, (byte) 1);
+        intent.putExtra("test", bytes);
+        startActivity(intent);
     }
 }
--- databindingdemo/src/main/java/com/example/databindingdemo/activity/RecyclerViewActivity.java
+++ databindingdemo/src/main/java/com/example/databindingdemo/activity/RecyclerViewActivity.java
@@ -3,6 +3,7 @@ package com.example.databindingdemo.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.databindingdemo.R;
 
@@ -12,5 +13,24 @@ public class RecyclerViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        Log.d("zhangyu1209", Log.getStackTraceString(new Throwable()));
+
+        //        at com.example.databindingdemo.activity.RecyclerViewActivity.onCreate(RecyclerViewActivity.java:17)
+        //        at android.app.Activity.performCreate(Activity.java:7136)
+        //        at android.app.Activity.performCreate(Activity.java:7127)
+        //        at android.app.Instrumentation.callActivityOnCreate(Instrumentation.java:1271)
+        //        at android.app.ActivityThread.performLaunchActivity(ActivityThread.java:2893)
+        //        at android.app.ActivityThread.handleLaunchActivity(ActivityThread.java:3048)
+        //        at android.app.servertransaction.LaunchActivityItem.execute(LaunchActivityItem.java:78)
+        //        at android.app.servertransaction.TransactionExecutor.executeCallbacks(TransactionExecutor.java:108)
+        //        at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:68)
+        //        at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1808)
+        //        at android.os.Handler.dispatchMessage(Handler.java:106)
+        //        at android.os.Looper.loop(Looper.java:193)
+        //        at android.app.ActivityThread.main(ActivityThread.java:6669)
+        //        at java.lang.reflect.Method.invoke(Native Method)
+        //        at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:493)
+        //        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:858)
     }
 }
